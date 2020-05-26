c48573f7248c014aa2d97b59705e317c100c0738
ChagOFF <chagovivan@mail.ru>
2020-05-26 11:11:55 +0700

    Дадова

---
+++ New Unity Project (2)/Assets/Scripts/MainScreen.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MainScreen : MonoBehaviour
+{
+	//public GameObject Settings;
+	public GameObject ClockSH; //Магаз Часов
+	public GameObject DestroySH; //Магаз Предметов
+	public GameObject BonuseSH;//Магаз Бонусов
+	public Text txt;
+	public int score = 0; //Общий счет
+	public int bonus = 1; //Прибавляемый бонус
+	void Start() //Все панели с магазами убираются
+	{
+		ClockSH.SetActive(false);
+		DestroySH.SetActive(false);
+		BonuseSH.SetActive(false);
+		//	Settings.SetActive(false);
+	}
+	private void Bonus(int BNS)//Получение нового значения бонуса из ClockShop
+	{
+		bonus = BNS;
+	}
+	public void OnMouseDown()//При нажатии на "Часы"
+	{
+		score += bonus;
+		txt.text = score + "$";
+	}
+	/*public void ClickOnSettings()
+    {
+		Settings.SetActive(true);
+    }*/
+	public void ClickOnClockShop() //При клике на Магаз часов
+	{
+		ClockSH.SetActive(!ClockSH.activeSelf);
+		DestroySH.SetActive(false);
+		BonuseSH.SetActive(false);
+	}
+	public void ClickOnDestroyShop() //При клике на магаз Предметов
+	{
+		DestroySH.SetActive(!DestroySH.activeSelf);
+		BonuseSH.SetActive(false);
+		ClockSH.SetActive(false);
+	}
+	public void ClickOnBonusShop() //При клике на магаз Бонусов
+	{
+		BonuseSH.SetActive(!BonuseSH.activeSelf);
+		ClockSH.SetActive(false);
+		DestroySH.SetActive(false);
+	}
+}
