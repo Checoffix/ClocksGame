985b9046939c22c192c24f31b33e685ba12fb6f1
Checoffix <vnikita.232003@gmail.com>
2020-06-02 13:12:00 +0700

    Создал новый проект, нужно исправить магазин часов, а также добавить хп часам

---
+++ TestZone/Assets/Scripts/Up5.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Up5 : MonoBehaviour
+{
+    public GameObject counter;
+    public double cost;
+    public Text text;
+    public GameObject but;
+    public void Update()
+    {
+        if (counter.GetComponent<Counter>().value >= 500)
+        {
+            but.SetActive(true);
+        }
+    }
+public void OnPressedButt()
+    {
+        if (counter.GetComponent<Counter>().value >= cost)
+        {
+            counter.GetComponent<Counter>().value -= cost;
+            counter.GetComponent<Counter>().currentValue += 5;
+            cost += 50;
+            text.GetComponent<Text>().text = "+5 валюты за нажатие. Цена: " + cost;
+        }
+    }
+}
