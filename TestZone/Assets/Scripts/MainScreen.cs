6a95fdebcf2ea2bba3ef2131f25a14f2d47fd2e9
Checoffix <vnikita.232003@gmail.com>
2020-06-03 22:47:32 +0700

    Перенёс панели в разные канвасы. Сделал выключение кнопки после своей покупки. Добавил кнопки возвращения из канвасов на основной канвас.

--- TestZone/Assets/Scripts/MainScreen.cs
+++ TestZone/Assets/Scripts/MainScreen.cs
@@ -8,19 +8,12 @@ public class MainScreen : MonoBehaviour
 {
 	//public GameObject Settings;
 	public GameObject Canvas;
-	public GameObject ClockSH; //Магаз Часов
-	public GameObject DestroySH; //Магаз Предметов
-	public GameObject BonuseSH;//Магаз Бонусов
 	public GameObject Tup;
 	public Text Counter;
 	public int score = 0; //Общий счет
 	public int bonus = 1; //Прибавляемый бонус
 	void Start() //Все панели с магазами убираются
-	{
-		ClockSH.SetActive(false);
-		DestroySH.SetActive(false);
-		BonuseSH.SetActive(false);
-		//	Settings.SetActive(false);
+	{ 
 	}
 	private void Bonus(int BNS)//Получение нового значения бонуса из ClockShop
 	{
@@ -42,27 +35,5 @@ public class MainScreen : MonoBehaviour
 			Tup = Instantiate(Tup);
 			Tup.transform.SetParent(Canvas.transform);
 		}
-	}
-	/*public void ClickOnSettings()
-    {
-		Settings.SetActive(true);
-    }*/
-	public void ClickOnClockShop() //При клике на Магаз часов
-	{
-		ClockSH.SetActive(!ClockSH.activeSelf);
-		DestroySH.SetActive(false);
-		BonuseSH.SetActive(false);
-	}
-	public void ClickOnDestroyShop() //При клике на магаз Предметов
-	{
-		DestroySH.SetActive(!DestroySH.activeSelf);
-		BonuseSH.SetActive(false);
-		ClockSH.SetActive(false);
-	}
-	public void ClickOnBonusShop() //При клике на магаз Бонусов
-	{
-		BonuseSH.SetActive(!BonuseSH.activeSelf);
-		ClockSH.SetActive(false);
-		DestroySH.SetActive(false);
 	}
 }
