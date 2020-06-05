5c4b23e2aa541344d6386b3f7c67891f3be0227b
ChagOFF <chagovivan@mail.ru>
2020-06-05 10:43:44 +0700

    Скрипт "Fly Sclae"

---
+++ TestZone/Assets/Scripts/FlyScale.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlyScale : MonoBehaviour
+{
+	private bool move;      //Создание движения
+	private Vector2 rand;   //Рандомный вектор
+	void Update()
+	{
+		if (!move) return; //Проверка на то, чтобы текст двигался
+		transform.Translate(rand * Time.deltaTime); //Перемещение текста в случайном направлении
+	}
+	public void StartMotion(float Score) //Задает всю инфу тексту
+	{
+		transform.localPosition = Vector2.zero;                         //Начальная позиция текста по нулям
+		GetComponent<Text>().text = "+" + Score;                        //То что выводит текст
+		rand = new Vector2(Random.Range(-3, 3), Random.Range(-3, 3));	//Задает случайное направление тексту
+		move = true;
+	}
+}
